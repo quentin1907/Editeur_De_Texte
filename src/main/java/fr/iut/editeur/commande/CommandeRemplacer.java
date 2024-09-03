0b1d5cd4172ebff95d981061356934278f4bad33
quentin <quentin.joly01@etu.umontpellier.fr>
2024-09-03 14:14:38 +0200

    correction executer CommandeRemplacer.java

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -11,7 +11,7 @@ public class CommandeRemplacer extends CommandeDocument {
     @Override
     public void executer() {
         String texte = "";
-        if (parameters.length == 2) {
+        if (parameters.length == 4) {
             texte = parameters[3];
         }
         this.document.remplacer(Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2]), texte);
