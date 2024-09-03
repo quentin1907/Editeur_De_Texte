ec62dc4a07a63db4e22e5a47c9e564bb90e073fe
quentin <quentin.joly01@etu.umontpellier.fr>
2024-09-03 14:14:15 +0200

    ajout CommandeRemplacer.java

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,20 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        String texte = "";
+        if (parameters.length == 2) {
+            texte = parameters[3];
+        }
+        this.document.remplacer(Integer.parseInt(parameters[1]),Integer.parseInt(parameters[2]), texte);
+        super.executer();
+    }
+}
